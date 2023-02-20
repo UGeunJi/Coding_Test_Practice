5d3f47da42b512f698d65490b5b328ae107e38a8
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-02-20 15:59:33 +0900

    Update [17087] 숨바꼭질 6.py

--- baekjoon/알고리즘 기초/[17087] 숨바꼭질 6.py
+++ baekjoon/알고리즘 기초/[17087] 숨바꼭질 6.py
@@ -1,15 +1,15 @@
 import math
 
-n,s  = map(int,input().split())
+n,s  = map(int, input().split())
 
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 
 distance = []
 for i in arr:
     distance.append(abs(s - i))
 
-ans=distance[0]
-for i in range(1,n):
-    ans=math.gcd(distance[i],ans)
+ans = distance[0]
+for i in range(1, n):
+    ans = math.gcd(distance[i], ans)
 
 print(ans)
