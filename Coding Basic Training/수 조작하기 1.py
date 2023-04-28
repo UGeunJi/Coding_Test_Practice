7123a841131df913283ae836ceb7076db85b5cd3
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-04-28 13:34:19 +0900

    Create 수 조작하기 1.py

---
+++ Coding Basic Training/수 조작하기 1.py
@@ -0,0 +1,12 @@
+def solution(n, control):
+    for i in control:
+        if i == 'w':
+            n += 1
+        elif i == 's':
+            n -= 1
+        elif i == 'd':
+            n += 10
+        else:
+            n -= 10
+            
+    return n
