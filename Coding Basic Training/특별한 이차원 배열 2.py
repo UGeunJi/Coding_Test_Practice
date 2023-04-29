ebfba82c11ab300441d34b4f6c10c334fc479e55
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-04-29 18:41:53 +0900

    Create 특별한 이차원 배열 2.py

---
+++ Coding Basic Training/특별한 이차원 배열 2.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+        else:
+            return 1
