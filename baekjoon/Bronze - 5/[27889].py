07cc184b601709d887e43fade619bc7ffb5732dc
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-05-07 22:56:56 +0900

    Create [27889].py

---
+++ baekjoon/Bronze - 5/[27889].py
@@ -0,0 +1,10 @@
+s = input()
+
+if s == 'NLCS':
+    print('North London Collegiate School')
+elif s == 'BHA':
+    print('Branksome Hall Asia')
+elif s == 'KIS':
+    print('Korea International School')
+elif s == 'SJA':
+    print('St. Johnsbury Academy')
