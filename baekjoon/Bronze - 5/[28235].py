c96f76bd0a62a06514192bfe6990a3a448c60649
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-06-18 21:31:21 +0900

    Create [28235].py

---
+++ baekjoon/Bronze - 5/[28235].py
@@ -0,0 +1,10 @@
+s = input()
+
+if s == 'SONGDO':
+    print('HIGHSCHOOL')
+elif s == 'CODE':
+    print('MASTER')
+elif s == '2023':
+    print('0611')
+elif s == 'ALGORITHM':
+    print('CONTEST')
