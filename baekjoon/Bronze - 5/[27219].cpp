c5f920f98b515aa4ed4d1ed10c37190b8faf8a4b
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-06-24 19:06:39 +0900

    Create [27219].cpp

---
+++ baekjoon/Bronze - 5/[27219].cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n / 5; i++) {
+        cout << "V";
+    }
+    
+    for (int i = 0; i < n % 5; i++) {
+        cout << "I";
+    }
+
+    return 0;
+}
