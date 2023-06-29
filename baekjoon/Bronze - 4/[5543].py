2013c07f20d51e15b2cd18c97c4308df0a52ce3e
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-06-30 00:58:57 +0900

    Create [5543].py

---
+++ baekjoon/Bronze - 4/[5543].py
@@ -0,0 +1,12 @@
+l1, l2 = [], []
+
+l1.append(int(input()))
+l1.append(int(input()))
+l1.append(int(input()))
+l2.append(int(input()))
+l2.append(int(input()))
+
+a = min(l1)
+b = min(l2)
+
+print(a + b - 50)
