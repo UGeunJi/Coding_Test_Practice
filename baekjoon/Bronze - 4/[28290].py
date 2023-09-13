e85f2f5d1f5d7b047507980145b7af42a2cdd61a
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-09-13 09:19:47 +0900

    Create [28290].py

---
+++ baekjoon/Bronze - 4/[28290].py
@@ -0,0 +1,12 @@
+s = input()
+
+if s == "fdsajkl;" or s == "jkl;fdsa":
+    print("in-out")
+elif s == "asdf;lkj" or s == ";lkjasdf":
+    print("out-in")
+elif s == "asdfjkl;":
+    print("stairs")
+elif s == ";lkjfdsa":
+    print("reverse")
+else:
+    print("molu")
