8fafa5aaeb4fd5c100c82a965dc5de9e0d3e55ce
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-09-21 07:09:35 +0900

    Create [30087].py

---
+++ baekjoon/Bronze - 5/[30087].py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(n):
+    s = input()
+    
+    if s = 'Algorithm':
+        print('204')
+    elif s = 'DataAnalysis':
+        print('207')
+    elif s = 'ArtificialIntelligence':
+        print('302')
+    elif s = 'CyberSecurity':
+        print('B101')
+    elif s = 'Network':
+        print('303')
+    elif s = 'Startup':
+        print('501')
+    elif s = 'TestStrategy':
+        print('105')
