bb22f5dc4b72fb6a2d346c723410104b889b909d
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-11-19 22:09:09 +0900

    Create [30676].py

---
+++ baekjoon/Bronze - 4/[30676].py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if 620 <= n <= 780:
+    print('Red')
+elif 590 <= n < 620:
+    print('Orange')
+elif 570 <= n < 590:
+    print('Yellow')
+elif 495 <= n < 570:
+    print('Green')
+elif 450 <= n < 495:
+    print('Blue')
+elif 425 <= n < 450:
+    print('Indigo')
+elif 380 <= n < 425:
+    print('Violet')
