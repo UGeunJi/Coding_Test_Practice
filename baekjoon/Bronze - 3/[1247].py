860cee6ae4e73e61f2ea69ff399164075105327c
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-12-22 11:30:31 +0900

    Update [1247].py

--- baekjoon/Bronze - 3/[1247].py
+++ baekjoon/Bronze - 3/[1247].py
@@ -1,6 +1,8 @@
+from sys import stdin
+
 for _ in range(3):
-    N = int(input())
-    li = [int(input()) for i in range(N)]
+    N = int(stdin.readline())
+    li = [int(stdin.readline()) for i in range(N)]
     if sum(li) == 0:
         print("0")
     elif sum(li) > 0:
