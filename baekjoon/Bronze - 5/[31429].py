5068a295a7e796a48b44ac67c82dd7063d3f85de
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-02-18 22:56:00 +0900

    Create [31429].py

---
+++ baekjoon/Bronze - 5/[31429].py
@@ -0,0 +1,23 @@
+n = int(input())
+if n == 1:
+    print(f'{12} {1600}')
+elif n == 2:
+    print(f'{11} {894}')
+elif n == 3:
+    print(f'{11} {1327}')
+elif n == 4:
+    print(f'{10} {1311}')
+elif n == 5:
+    print(f'{9} {1004}')
+elif n == 6:
+    print(f'{9} {1178}')
+elif n == 7:
+    print(f'{9} {1357}')
+elif n == 8:
+    print(f'{8} {837}')
+elif n == 9:
+    print(f'{7} {1055}')
+elif n == 10:
+    print(f'{6} {556}')
+elif n == 11:
+    print(f'{6} {773}')
