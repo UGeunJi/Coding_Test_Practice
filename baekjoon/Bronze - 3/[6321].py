c11a4d9a3d63da13ac6fa1b087d1f4a58a501479
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-03-31 23:42:14 +0900

    Create [6321].py

---
+++ baekjoon/Bronze - 3/[6321].py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for x in range(1, n+1) :
+  data = input()
+  result = ''
+  for i in range(len(data)) :
+    value = ord(data[i]) + 1
+    if value > 90 :
+      value = 65
+    result += chr(value)
+    
+  print('String #%d' % x)
+  print(result)
+  print()
