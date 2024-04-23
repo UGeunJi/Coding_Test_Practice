a3662eff441e3b78b43470996e4e054b7b6d8986
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-04-23 15:58:07 +0900

    Create [30642].py

---
+++ baekjoon/Bronze - 3/[30642].py
@@ -0,0 +1,14 @@
+n = int(input())
+a = input()
+k = int(input())
+
+if a == "annyong":
+    if k % 2 == 1:
+        print(K)
+    else:
+        print(k + 1 if k + 1 <= n else k - 1)
+elif a == "induck":
+    if k % 2 == 0:
+        print(K)
+    else:
+        print(k + 1 if K + 1 <= n else k - 1)
