ebd32aac09c64e1cd0e5dbb5ca6ec18345c768ad
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-04-23 15:58:54 +0900

    Update [30642].py

--- baekjoon/Bronze - 3/[30642].py
+++ baekjoon/Bronze - 3/[30642].py
@@ -4,11 +4,11 @@ k = int(input())
 
 if a == "annyong":
     if k % 2 == 1:
-        print(K)
+        print(k)
     else:
         print(k + 1 if k + 1 <= n else k - 1)
 elif a == "induck":
     if k % 2 == 0:
-        print(K)
+        print(k)
     else:
-        print(k + 1 if K + 1 <= n else k - 1)
+        print(k + 1 if k + 1 <= n else k - 1)
