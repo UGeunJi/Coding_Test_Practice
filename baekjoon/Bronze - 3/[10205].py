789bbf114c16b33099e0a424e58c64d267cfa48b
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-05-07 19:34:36 +0900

    Create [10205].py

---
+++ baekjoon/Bronze - 3/[10205].py
@@ -0,0 +1,8 @@
+for n in range(1, int(input()) + 1):
+    h = int(input())
+    for i in input():
+        if i == "c": 
+            h += 1
+        else: 
+            h -= 1
+    print(f"Data Set {n}:\n{h}\n")
