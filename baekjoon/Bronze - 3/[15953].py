a14544eb26be0d7dc464f2a522ecd005c2380a90
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-06-11 10:05:50 +0900

    Create [15953].py

---
+++ baekjoon/Bronze - 3/[15953].py
@@ -0,0 +1,33 @@
+t = int(input())
+
+def fastival1(score):
+    if score == 1:
+        return 5000000
+    elif 1 < score <= 3:
+        return 3000000
+    elif 3 < score <= 6:
+        return 2000000
+    elif 6 < score <= 10:
+        return 500000
+    elif 10 < score <= 15:
+        return 300000
+    elif 15 < score <= 21:
+        return 100000
+    else:
+        return 0
+def fastival2(score):
+    if score == 1:
+        return 5120000
+    elif 1 < score <= 3:
+        return 2560000
+    elif 3 < score <= 7:
+        return 1280000
+    elif 7 < score <= 15:
+        return 640000
+    elif 15 < score <= 31:
+        return 320000
+    else:
+        return 0
+for _ in range(t):
+    first, second = map(int, input().split(' '))
+    print(fastival1(first) + fastival2(second))
