781fd6b231dfc09c95a4b7db822cf2c91336fb18
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-10-31 16:33:20 +0900

    Create [23811].py

---
+++ baekjoon/Bronze - 3/[23811].py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for _ in range(n):
+    print('@'*(5 * n))
+
+for _ in range(n):
+    print('@' * n)
+
+for _ in range(n):
+    print('@'*(5 * n))
+
+for _ in range(n):
+    print('@' * n)
+
+for _ in range(n):
+    print('@'*(5 * n))
