2d4476b1bdb4497d97371dfbbaff37f11bcb4ac2
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-11-12 10:10:08 +0900

    Update [10101].py

--- baekjoon/Bronze - 4/[10101].py
+++ baekjoon/Bronze - 4/[10101].py
@@ -2,11 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a == 60 and b == 60 and c == 60:
+if a + b + c != 180:
+    print('Error')
+elif a + b + c == 180 and a == 60 and b == 60 and c == 60:
     print('Equilateral')
-elif a + b + c == 180 and a == b or a == c or b == c:
-    print('Isosceles')
-elif a + b + c == 180 and a != b and a != c and b != c:
+elif a + b + c == 180 and a != b and b != c and c != a:
     print('Scalene')
 else:
-    print('Error')
+    print('Isosceles')
