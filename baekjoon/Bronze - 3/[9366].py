39d987a2eda0ac8bed65f54cc9f642432444bbce
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-11-15 10:32:47 +0900

    Create [9366].py

---
+++ baekjoon/Bronze - 3/[9366].py
@@ -0,0 +1,12 @@
+for case in range(int(input())):
+    l = sorted(map(int, input().split()))
+    print(f"Case #{case + 1}: ", end='')
+    
+  if li[0] + li[1] <= li[2]:
+        print("invalid!")
+    elif li[0] == li[1] == li[2]:
+        print("equilateral")
+    elif li[0] == li[1] or li[1] == li[2] or li[2] == li[0]:
+        print("isosceles")
+    else:
+        print("scalene")
