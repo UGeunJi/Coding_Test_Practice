87dccaa493a4599ee2a5f5262e590461d27ca465
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2025-03-18 11:31:54 +0900

    Create [14761].py

---
+++ baekjoon/Bronze - 3/[14761].py
@@ -0,0 +1,11 @@
+x, y, n = map(int, input().split())
+
+for i in range(1, n + 1):
+    if i % x == 0 and i % y == 0:
+        print("FizzBuzz")
+    elif i % x == 0 :
+        print("Fizz")
+    elif i % y == 0 :
+        print("Buzz")
+    else:
+        print(i)
