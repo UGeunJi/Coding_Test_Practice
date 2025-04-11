342759506f0835e78fe37eece374728fc25316a5
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2025-04-11 15:07:11 +0900

    Create [13240].py

---
+++ baekjoon/Bronze - 3/[13240].py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+for i in range(n):
+    for j in range(m):
+        if (i + j) % 2 == 0:
+            print('*', end='')
+        else:
+            print('.', end='')
+    print()
