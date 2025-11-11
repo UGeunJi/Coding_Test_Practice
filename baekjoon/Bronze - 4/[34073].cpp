e67cb0a1b910b1ae8e9061d19895c3e43a59a762
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2025-11-11 16:39:37 +0900

    Create [34073].cpp

---
+++ baekjoon/Bronze - 4/[34073].cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int N;
+string str;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    cin >> N;
+
+    while (N--)
+    {
+        cin >> str;
+        cout << str << "DORO" << " ";
+    }
+    return 0;
+}
