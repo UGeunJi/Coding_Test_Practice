6ae4e2887d8a88126d1935bc8168eaa888df42aa
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2026-01-14 20:49:35 +0900

    Create [34691].py

---
+++ baekjoon/Bronze - 5/[34691].py
@@ -0,0 +1,10 @@
+while True:
+    s = input()
+    if s == 'end':
+        break
+    if s == 'animal':
+        print('Panthera tigris')
+    elif s == 'tree':
+        print('Pinus densiflora')
+    elif s == 'flower':
+        print('Forsythia koreana')
