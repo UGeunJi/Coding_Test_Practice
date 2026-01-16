28f97d0f627ac4c235d86f0b16d651a2c0543f7c
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2026-01-16 15:12:02 +0900

    Create [32154].py

---
+++ baekjoon/Bronze - 5/[32154].py
@@ -0,0 +1,32 @@
+n = int(input())
+
+if n == 1:
+    print(11)
+    print('A B C D E F G H J L M')
+elif n == 2:
+    print(9)
+    print('A C E F G H I L M')
+elif n == 3:
+    print(9)
+    print('A C E F G H I L M')
+elif n == 4:
+    print(9)
+    print('A B C E F G H L M')
+elif n == 5:
+    print(8)
+    print('A C E F G H L M')
+elif n == 6:
+    print(8)
+    print('A C E F G H L M')
+elif n == 7:
+    print(8)
+    print('A C E F G H L M')
+elif n == 8:
+    print(8)
+    print('A C E F G H L M')
+elif n == 9:
+    print(8)
+    print('A C E F G H L M')
+elif n == 10:
+    print(8)
+    print('A B C F G H L M')
