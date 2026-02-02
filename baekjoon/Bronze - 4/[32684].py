4817a5656e66e139df95cac00caf6c695b92bc8b
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2026-02-03 03:39:18 +0900

    Create [32684].py

---
+++ baekjoon/Bronze - 4/[32684].py
@@ -0,0 +1,10 @@
+a, b, c, d, e, f = map(int, input().split())
+aa = a * 13 + b * 7 + c * 5 + d * 3 + e * 3 + f * 2
+
+a, b, c, d, e, f = map(int, input().split())
+bb = a * 13 + b * 7 + c * 5 + d * 3 + e * 3 + f * 2 + 1.5
+
+if aa > bb:
+    print('cocjr0208')
+else:
+    print('ekwoo')
