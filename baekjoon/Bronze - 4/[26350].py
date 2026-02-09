e9cce23403e8a3781aa53b79651c9b9e38c4bccc
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2026-02-09 13:34:26 +0900

    Create [26350].py

---
+++ baekjoon/Bronze - 4/[26350].py
@@ -0,0 +1,14 @@
+n = int(input())
+l_temp = []
+
+for i in range(n):
+    l_temp = list(map(int, input().split()))
+    for j in range(len(l_temp) - 2):
+        if l_temp[j + 1] * 2 <= l_temp[j + 2]:
+            if j == len(l_temp) - 3:
+                print('Denominations:', *l_temp[1:])
+                print('Good coin denominations!')
+        else:
+            print('Denominations:', *l_temp[1:])
+            print('Bad coin denominations!')
+            break
